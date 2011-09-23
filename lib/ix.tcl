922b83520987bdc546b792108d4c64be4f9d5f97
Jay Adkisson <jjmadkisson@gmail.com>
2011-09-23 15:00:38 -0700

    use package require

--- lib/ix.tcl
+++ lib/ix.tcl
@@ -1,4 +1,6 @@
-source [file dirname [info script]]/../deps/jim-ncurses/lib/ncurses.tcl
+lappend auto_path [file dirname [info script]]/../deps/jim-ncurses/lib
+
+package require ncurses
 
 proc ix.run {} {
   ncurses.do {
