87f433773fdc503d6c2106428b658ccb6ce9fe87
Jordan Salter <rhyme@ndreason.com>
2011-09-23 22:02:12 -0700

    Windows:  display buffers.

--- lib/window.tcl
+++ lib/window.tcl
@@ -4,6 +4,7 @@ class Window {
     win ""
     buffer ""
     dimensions {}
+    firstline 0
     cursor {}
 }
 
@@ -15,7 +16,11 @@ Window method show {} {
     ncurses.do {
         set win [stdscr window {*}[stdscr getmaxyx] 0 0]
         $win box
-        $win puts {1 1} [$buffer getBuffer]
+
+        for {set i 0} {$i < [lindex [$win getmaxyx] 0]} {incr i} {
+            $win puts [list [+ 1 $i] 1] [lindex [$buffer getBuffer] [+ $i $firstline]]
+        }
+
         ncurses.getc
     }
 }
